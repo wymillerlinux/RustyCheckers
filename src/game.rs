f32fe3441294f54d1d2699f2245bea56c3550a11
Wyatt J. Miller <wyatt@wyattjmiller.com>
2022-10-08 12:18:38 -0400

    added board and game logic

---
+++ src/game.rs
@@ -0,0 +1,14 @@
+use super::board::{Coordinate, GamePiece, Move, PieceColor};
+
+pub struct GameEngine {
+    board: [[Option<GamePiece>; 8]; 8],
+    current_turn: PieceColor,
+    move_count: u32,
+}
+
+pub struct MoveResult {
+    pub mv: Move,
+    pub crowned: bool,
+}
+
+
